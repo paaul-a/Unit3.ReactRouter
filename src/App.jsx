4c62de2468a88ff2e5520529cbe33892632fafd7
Paula Mason <paulamason@Paulas-MBP.hsd1.ga.comcast.net>
2023-11-09 14:28:55 -0500

    finished baseline workshop

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Routes, Route, Link } from 'react-router-dom' 
+import Blue from './components/Blue'
+import Red from './components/Red'
+import Home from './components/Home'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div id="container">
+      {/* <h1>Hello React Router!</h1> */}
+      <div id="navbar">
+        <Link to='/'>Home</Link>
+        <Link to='/blue'>Blue</Link>
+        <Link to='/red'>Red</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/blue' element={<Blue />} />
+          <Route path='/red' element={<Red />} />
+        </Routes>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+
+
+function Home() {
+  return (
+    <>
+      <div className="home">
+        <h1>HOME</h1>
+      </div>
+    
+    </>
+  )
+
+}
+
+export default Home
